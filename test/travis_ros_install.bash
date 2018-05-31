8fc8d46fc7e9fd09ae6c67dfc82ea256e48bfe98
Shohei Aoki <shoaok@gmail.com>
2018-06-01 04:50:32 +0900

    add travis config

---
+++ test/travis_ros_install.bash
@@ -0,0 +1,19 @@
+#!/bin/bash -xve
+
+pip install catkin_pkg
+pip install empy
+pip install pyyaml
+pip install rospkg
+
+cd ..
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
+cd ./ros_setup_scripts_Ubuntu14.04_server
+bash ./step0.bash
+bash ./step1.bash
+
+mkdir -p ~/catkin_ws/src 
+cd ~/catkin_ws/src
+source /opt/ros/indigo/setup.bash
+catkin_init_workspace
+cd ~/catkin_ws
+catkin_make
